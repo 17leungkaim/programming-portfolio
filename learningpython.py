23d5219921c73a446b71c3f76f1fb8bfb2248020
Kainoa Leung Choi <kainoaleungchoi@Alveyworld-26.local>
2015-09-15 11:05:35 -0600

    learning python

---
+++ learningpython.py
@@ -0,0 +1,8 @@
+# Learning Python
+print "Hello World!"
+
+number = int(raw_input("Enter an integer:"))
+if number < 100:
+	print "Your number is smaller than 100"
+else:
+	print "Your number is greater than 100"
