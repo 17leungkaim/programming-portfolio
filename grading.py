bd6988fe532e5f5b005169d01f3912bd5c2834ac
Kainoa Leung Choi <kainoaleungchoi@Alveyworld-26.local>
2015-09-15 11:04:56 -0600

    Grading Only

---
+++ grading.py
@@ -0,0 +1,28 @@
+# program to figure out grades
+
+score = int(raw_input("Enter your test score"))
+
+if score >= 93:
+	print "A"
+elif score >= 90:
+	print "-A"
+elif score >= 87:
+	print "B+"
+elif score >=83:
+	print "B"
+elif score >=80:
+	print "-B"
+elif score >=77:
+	print "+C"
+elif score >=73:
+	print "C"	
+elif score >=70:
+	print "-C"
+elif score >=67:
+	print "+D"
+elif score >=63:
+	print "D"
+elif score >=60:
+	print "-D"
+elif score >=50:
+	print "F"
