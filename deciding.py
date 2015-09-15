e4eddeebaa3ac849b475666d27c70bb27d478902
Kainoa Leung Choi <kainoaleungchoi@Alveyworld-26.local>
2015-09-15 11:04:15 -0600

    hello world

---
+++ deciding.py
@@ -0,0 +1,14 @@
+# Learning if statements
+
+number = int(raw_input("Enter your age"))
+
+if number < 5:
+	print "You know nothing"
+elif number < 11
+	print "You know little"
+elif number <15
+	print "You are getting a bit smarter"
+elif number <19
+	print "you are smart"
+else:
+	print "You know everthing"
