8b0e4e197f2261304f5c291e2a3f98dd0e47781a
Kainoa Leung Choi <kainoaleungchoi@Alveyworld-26.local>
2016-01-07 10:51:13 -0700

    Try it

---
+++ madlib.py
@@ -0,0 +1,24 @@
+# mad lib
+
+adj1 = raw_input("Enter an adjective: ")
+adj2 = raw_input("Enter an adjective: ")
+bird = raw_input("Enter a type of bird: ")
+room = raw_input("Enter a type of room: ")
+verb1 = raw_input("Enter a verb (past tense): ")
+verb2 = raw_input("Enter a verb: ")
+name = raw_input("Enter relative's name: ")
+noun1 = raw_input("Enter noun: ")
+liq = raw_input("Enter favorite drink: ")
+verb3 = raw_input("Enter verb ending with -ing: ")
+bdy = raw_input("Enter part of the body (Plural): ")
+plur = raw_input("Enter Plural noun: ")
+verb4 = raw_input("Enter verb ending with -ing: ")
+noun2 = raw_input("Enter noun: ")
+
+print "It was a " + adj1 + ", cold November day."
+print "I woke up to the " + adj2 + " smell of " + bird + " roasting in the " + room + " downstairs."
+print "I " + verb1 + " down the stairs to see if I could help " + verb2 + " the dinner."
+print "My mom said, 'See if " + name + " needs a fresh " + noun1 + ".'"
+print "So I carried a tray of glasses full of " + liq + " into the " + verb3 + " room."
+print "When I got there, I couldn't believe my " + bdy + "!"
+print "There were " + plur + "," + verb4 + " on the " + noun2 + "!"
