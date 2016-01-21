dc1d50c3eed6aa1bd14cf92ef9c1c16603608681
Kainoa Leung Choi <kainoaleungchoi@Alveyworld-26.local>
2016-01-21 09:59:45 -0700

    games

---
+++ guessinggame.py
@@ -0,0 +1,30 @@
+#High low guessing game
+
+
+print ("Pick a number between 1 and 1000 and I will try to guess it. I can do this in no more than 10 guesses.")
+print ("After each guess, enter:")
+print ("0 - if I got it right")
+print ("-1 - if I guessed too high")
+print ("1 - if I guessed too low")
+	
+def take_guess(a, b):
+	guess is 500
+	flag is 1
+	while flag is 1:
+		print 500
+		raw_input("Enter: ")
+		if 0:
+			print "I got it right"
+			set keep asking flag to 0
+		else if -1:
+			print "I will guess lower"
+			set keep asking flag to 0
+		else if 1:
+			print "I will guess higher"
+			set keep asking flag to 0
+		else:
+			print ("0 - if I got it right")
+			print ("-1 - if I guessed too high")
+			print ("1 - if I guessed too low")
+	print blank
+	
