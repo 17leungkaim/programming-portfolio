aa0f05fd56228e8e8a706f1e72032dabc29d17ac
Kainoa Leung Choi <kainoaleungchoi@Alveyworld-26.local>
2016-01-29 09:28:01 -0700

    Not ready yet

--- The_Game_of_Pig.py
+++ The_Game_of_Pig.py
@@ -3,5 +3,53 @@
 import random
 
 def roll_die(sides):
-	r = random.randrange(1, sides+1)
+	r = random.randrange(1, 6)
 	return r
+	
+
+def take_turn(point):
+	point = 0
+	return r
+	P1 = raw_input("Player 1 press Enter to begin your turn: "	)
+	
+	return P1
+	
+	while P1:
+		return roll_die
+		if roll_die is 1:
+			
+			
+		else:
+			return roll_die
+			Qst = raw_input("Would you like to continue: ")
+			return Qst
+			if yes:
+				return roll_die
+			else:
+				return P2 roll_die
+print"End of turn"
+return number of turn points			
+
+def show_instructions():
+print "Welcom to the Game of Pig. To win, be the player with the most points at the end of the game. the game ends at the end of a round where at least one plaer has 100 or more points."
+	print "On each turn, you may roll the die as many times as you like to obtain more points.  However, if you roll a 1, your turn is over, and you do not obtain any points that turn."
+
+def main():
+		return show_instructions
+		Pl1 = 0
+		Pl2 = 0
+		while Player1 and Player2 total points are < 100:
+			return Pl1 + Pl2
+			return Pl1 + roll_die
+			
+			
+			
+			
+		return "Game Over"	
+		return Pl1 + Pl2
+		if Pl1 > Pl2:
+			return "You win"
+		elif not:
+			return "You lose"
+		else:
+			return "Its a tie"
